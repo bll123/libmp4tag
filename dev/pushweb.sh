79df71d49160135d3a52b878fed6a647ec887541
Brad Lanam <brad.lanam.di@gmail.com>
2023-08-24 14:15:57 -0700

    website

---
+++ dev/pushweb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SFUSER=bll123
+
+echo -n "sourceforge Password: "
+read -s SSHPASS
+echo ""
+if [[ $SSHPASS == "" ]]; then
+  echo "No password."
+  exit 1
+fi
+export SSHPASS
+
+project=libmp4tag
+server=web.sourceforge.net
+remuser=bll123
+port=22
+wwwpath=/home/project-web/${project}/htdocs
+
+VERSFN=tmp/vers.txt
+VERS=$(cat ${VERSFN})
+TMP=webtmp
+
+test -d ${TMP} && rm -rf ${TMP}
+mkdir -p ${TMP}
+
+sed "s/#VERSION#/${VERS}/" dev/${project}.html > ${TMP}/index.html
+
+if [[ ! -d $TMP ]]; then
+  echo "no $TMP"
+  exit 1
+fi
+
+cd $TMP
+sshpass -e rsync -v -e ssh -aS \
+    . \
+    ${remuser}@${server}:${wwwpath}
+cd ..
+rm -rf ${TMP}
+
+exit 0
