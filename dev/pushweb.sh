29e8fc7197c15667ef9d9e3e1a68b78493da6c77
Brad Lanam <brad.lanam.di@gmail.com>
2023-11-29 10:31:53 -0800

    icon; re-work web page

--- dev/pushweb.sh
+++ dev/pushweb.sh
@@ -24,7 +24,18 @@ TMP=webtmp
 test -d ${TMP} && rm -rf ${TMP}
 mkdir -p ${TMP}
 
+inkscape dev/${project}.svg -w 16 -h 16 -o ${project}-16.png > /dev/null 2>&1
+icotool -c -o favicon.ico ${project}-16.png
+mv favicon.ico ${TMP}
+rm -f ${project}-16.png
+inkscape dev/${project}.svg -w 128 -h 128 -o ${project}.png > /dev/null 2>&1
+mv ${project}.png ${TMP}
 sed "s/#VERSION#/${VERS}/" dev/${project}.html > ${TMP}/index.html
+touch -r dev/${project}.html ${TMP}/index.html
+if [[ $VERSFN -nt dev/${project}.html ]]; then
+  touch -r $VERSFN ${TMP}/index.html
+fi
+cp -p dev/libmp4tag.svg ${TMP}
 
 if [[ ! -d $TMP ]]; then
   echo "no $TMP"
@@ -32,7 +43,7 @@ if [[ ! -d $TMP ]]; then
 fi
 
 cd $TMP
-sshpass -e rsync -v -e ssh -aS --delete \
+sshpass -e rsync -c -vv -e ssh -aS --delete \
     . \
     ${remuser}@${server}:${wwwpath}
 cd ..
