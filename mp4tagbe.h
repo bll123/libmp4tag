61fb29ceca52b2ff22713db91ffd16eeeaeb22b8
Brad Lanam <brad.lanam.di@gmail.com>
2026-01-20 08:11:51 -0800

    fix various build issues b

--- mp4tagbe.h
+++ mp4tagbe.h
@@ -25,39 +25,5 @@
 # define htobe32 htonl
 # define htobe16 htons
 # define htobe64 htonll
-
-#if 0
-# if defined (__cplusplus) || defined (c_plusplus)
-extern "C" {
-# endif
-
-# define be32toh ntohl
-# define be16toh ntohs
-
-/* it appears that the msys2 winsock2 header file */
-/* does not define ntohll or htonll */
-/* but this will get all mucked up if ntohll is actually defined */
-static inline uint64_t
-ntohll (uint64_t v)
-{
-  return (((uint64_t) ntohl((uint32_t) (v & 0xFFFFFFFFUL))) << 32) |
-      (uint64_t) ntohl ((uint32_t) (v >> 32));
-}
-# define be64toh ntohll
-# define htobe32 htonl
-# define htobe16 htons
-static inline uint64_t
-htonll (uint64_t v)
-{
-  return (((uint64_t) htonl((uint32_t) (v & 0xFFFFFFFFUL))) << 32) |
-      (uint64_t) htonl ((uint32_t) (v >> 32));
-}
-# define htobe64 htonll
-
-# if defined (__cplusplus) || defined (c_plusplus)
-} /* extern C */
-# endif
-#endif
-
 #endif
 
