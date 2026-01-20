4ede82656d2392d79f8d6e3f0938664d25ae4335
Brad Lanam <brad.lanam.di@gmail.com>
2026-01-20 08:23:32 -0800

    new check for nodiscard

--- nodiscard.h
+++ nodiscard.h
@@ -3,19 +3,15 @@
  */
 #pragma once
 
+#include "config.h"
+
 #if __STDC_VERSION__ < 202000
 # undef NODISCARD
 # define NODISCARD __attribute__ ((warn_unused_result))
 #endif
-#if __STDC_VERSION__ >= 202000
+#if _has_nodiscard
 # undef NODISCARD
 # define NODISCARD [[nodiscard]]
-#endif
-#if __STDC_VERSION__ >= 202000 && defined (__has_cpp_attribute)
-# if ! defined (NODISCARD) && __has_cpp_attribute( nodiscard )
-#  undef NODISCARD
-#  define NODISCARD [[nodiscard]]
-# endif
 #endif
 #if ! defined (NODISCARD)
 # define NODISCARD
